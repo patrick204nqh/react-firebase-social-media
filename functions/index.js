e7dfb560ce3c23fd13feb6bf9d9677147513d964
patrick <patrick204nqh@gmail.com>
2020-08-08 01:10:30 +0700

    write user registration to database

--- functions/index.js
+++ functions/index.js
@@ -68,6 +68,7 @@ app.post('/signup', (req, res) => {
   }
 
   // TODO: validate data
+  let token, userId;
   db.doc(`/users/${newUser.handle}`).get()
     .then(doc => {
       if (doc.exists) {
@@ -81,23 +82,29 @@ app.post('/signup', (req, res) => {
       }
     })
     .then(data => {
+      console.log(data);
+      userId = data.user.uid;
       return data.user.getIdToken()
     })
-    .then(token => {
+    .then(idToken => {
+      token = idToken;
+      const userCredentials = {
+        handle: newUser.handle,
+        email: newUser.email,
+        createAt: new Date().toISOString(),
+        userId
+      };
+      return db.doc(`/users/${newUser.handle}`).set(userCredentials);
+    })
+    .then(() => {
       return res.status(201).json({ token });
     })
     .catch(err => {
-      console.log(err);
-      return res.status(500).json({ error: err.code })
-    })
-
-  firebase.auth().createUserWithEmailAndPassword(newUser.email, newUser.password)
-    .then((data) => {
-      return res.status(201).json({ message: `user ${data.user.uid} signed up successfully` })
-    })
-    .catch(err => {
-      console.log(err)
-      return res.status(500).json({ error: err.code })
+      if (err.code === 'auth/email-already-in-use') {
+        return res.status(400).json({ email: 'Email is already is use' })
+      } else {
+        return res.status(500).json({ error: err.code })
+      }
     })
 })
 

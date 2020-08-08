5e7c3fc3a9e7ebae97aa76334d8fbb98a8e3dd68
patrick <patrick204nqh@gmail.com>
2020-08-08 15:10:10 +0700

    authentication middleware (auth headers request)

--- functions/index.js
+++ functions/index.js
@@ -2,6 +2,7 @@ const firebase = require('firebase');
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 const express = require('express');
+const { decode } = require('firebase-functions/lib/providers/https');
 const app = express();
 
 const firebaseConfig = {
@@ -39,9 +40,38 @@ app.get('/screams', (req, res) => {
     .catch(err => console.log(err));
 })
 
-app.post('/scream', (req, res) => {
+const FBAuth = (req, res, next) => {
+  let idToken;
+  if (req.headers.authorization && req.headers.authorization.startsWith('Bearer ')) {
+    idToken = req.headers.authorization.split('Bearer ')[1];
+  } else {
+    console.log('No token found');
+    return res.status(403).json({ error: 'Unauthorized' });
+  }
+
+  admin.auth().verifyIdToken(idToken)
+    .then(decodedToken => {
+      req.user = decodedToken;
+      // console.log(decodedToken);
+      return db.collection('users')
+        .where('userId', '==', req.user.uid)
+        .limit(1)
+        .get();
+    })
+    .then(data => {
+      req.user.handle = data.docs[0].data().handle;
+      return next();
+    })
+    .catch(err => {
+      console.error('Error while verifying token ', err);
+      return res.status(403).json(err);
+    })
+}
+
+// Post one scream
+app.post('/scream', FBAuth, (req, res) => {
   const newScream = {
-    userHandle: req.body.userHandle,
+    userHandle: req.user.handle,
     body: req.body.body,
     createAt: new Date().toISOString()
   }

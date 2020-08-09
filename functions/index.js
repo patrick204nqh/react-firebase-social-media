b892f1925e5049000f0db0930bdf07220a464b25
patrick <patrick204nqh@gmail.com>
2020-08-10 02:12:48 +0700

    image upload

--- functions/index.js
+++ functions/index.js
@@ -2,14 +2,15 @@ const functions = require('firebase-functions');
 const app = require('express')();
 
 const { getAllScreams, postOneScream } = require('./handlers/screams');
-const { signup, login } = require('./handlers/users');
-const fbAuth = require('./util/fbAuth');
+const { signup, login, uploadImage } = require('./handlers/users');
+const FBAuth = require('./util/fbAuth');
 
 // screams routes
 app.get('/screams', getAllScreams);
-app.post('/scream', fbAuth, postOneScream);
+app.post('/scream', FBAuth, postOneScream);
 // users routes
 app.post('/signup', signup);
 app.post('/login', login);
+app.post('/user/image', FBAuth, uploadImage)
 
 exports.api = functions.https.onRequest(app);

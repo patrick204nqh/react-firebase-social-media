6c95560ba60d97496668be56c3c5312f98053aab
patrick <patrick204nqh@gmail.com>
2020-08-13 11:52:26 +0700

    material-ui with routes

--- client/src/App.js
+++ client/src/App.js
@@ -6,16 +6,20 @@ import './App.css';
 import Home from './pages/home';
 import Login from './pages/login';
 import Signup from './pages/signup';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
       <Router>
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route exact path="/login" component={Login} />
-          <Route exact path="/signup" component={Signup} />
-        </Switch>
+        <Navbar />
+        <div className="container">
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/login" component={Login} />
+            <Route exact path="/signup" component={Signup} />
+          </Switch>
+        </div>
       </Router>
     </div>
   );

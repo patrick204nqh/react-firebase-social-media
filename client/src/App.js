fe69ee0ac8ada922cb7c31905465bef38b99b090
patrick <patrick204nqh@gmail.com>
2020-08-13 10:40:29 +0700

    switch routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,22 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import './App.css';
+
+// Pages
+import Home from './pages/home';
+import Login from './pages/login';
+import Signup from './pages/signup';
 
 function App() {
   return (
     <div className="App">
-      <h1>hello, world</h1>
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/signup" component={Signup} />
+        </Switch>
+      </Router>
     </div>
   );
 }

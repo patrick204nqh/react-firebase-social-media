ba7ba63c4e010af8bcc2bcedec19d9ea839926f3
patrick <patrick204nqh@gmail.com>
2020-08-13 19:29:02 +0700

    get screams from back-end

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import './App.css';
+import { MuiThemeProvider } from '@material-ui/core/styles';
+import createMuiTheme from '@material-ui/core/styles/createMuiTheme';
 
 // Pages
 import Home from './pages/home';
@@ -8,20 +10,39 @@ import Login from './pages/login';
 import Signup from './pages/signup';
 import Navbar from './components/Navbar';
 
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      light: '#33c9dc',
+      main: '#00bcd4',
+      dark: '#008394',
+      contrastText: '#fff'
+    },
+    secondary: {
+      light: '#ff6333',
+      main: '#ff3d00',
+      dark: '#b22a00',
+      contrastText: '#fff'
+    }
+  },
+});
+
 function App() {
   return (
-    <div className="App">
-      <Router>
-        <Navbar />
-        <div className="container">
-          <Switch>
-            <Route exact path="/" component={Home} />
-            <Route exact path="/login" component={Login} />
-            <Route exact path="/signup" component={Signup} />
-          </Switch>
-        </div>
-      </Router>
-    </div>
+    <MuiThemeProvider theme={theme} >
+      <div className="App">
+        <Router>
+          <Navbar />
+          <div className="container">
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route exact path="/login" component={Login} />
+              <Route exact path="/signup" component={Signup} />
+            </Switch>
+          </div>
+        </Router>
+      </div>
+    </MuiThemeProvider>
   );
 }
 
--- client/src/pages/home.js
+++ client/src/pages/home.js
@@ -1,11 +1,31 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import axios from 'axios';
+import { Grid } from '@material-ui/core';
 
 class Home extends Component {
+  state = {
+    screams: null
+  }
+  componentDidMount() {
+    axios.get('/screams')
+      .then(res => {
+        console.log(res.data);
+        this.setState({
+          screams: res.data
+        })
+      })
+      .catch(err => console.log(err));
+  }
   render() {
     return (
-      <div>
-        Home page
-      </div>
+      <Grid container spacing={0}>
+        <Grid item sm={8} xs={12}>
+          <p>Content...</p>
+        </Grid>
+        <Grid item sm={4} xs={12}>
+          <p>Profile...</p>
+        </Grid>
+      </Grid>
     )
   }
 }

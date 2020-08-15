ccf89c951a3c03316316fe1419223f2ea217b9c9
patrick <patrick204nqh@gmail.com>
2020-08-16 03:05:09 +0700

    auth routes state variable

--- client/src/App.js
+++ client/src/App.js
@@ -4,14 +4,28 @@ import './App.css';
 import { MuiThemeProvider } from '@material-ui/core/styles';
 import createMuiTheme from '@material-ui/core/styles/createMuiTheme';
 import themeFile from './util/theme';
+import jwtDecode from 'jwt-decode';
 // Components
 import Navbar from './components/Navbar';
+import AuthRoute from './util/AuthRoute';
 // Pages
 import Home from './pages/home';
 import Login from './pages/login';
 import Signup from './pages/signup';
 
 const theme = createMuiTheme(themeFile);
+
+let authenticated;
+const token = localStorage.FBIdToken;
+if (token) {
+  const decodedToken = jwtDecode(token);
+  if (decodedToken.exp * 1000 < Date.now()) {
+    window.location.href = '/login';
+    authenticated = false;
+  } else {
+    authenticated = true;
+  }
+}
 
 function App() {
   return (
@@ -22,8 +36,18 @@ function App() {
           <div className="container">
             <Switch>
               <Route exact path="/" component={Home} />
-              <Route exact path="/login" component={Login} />
-              <Route exact path="/signup" component={Signup} />
+              <AuthRoute
+                exact
+                path="/login"
+                component={Login}
+                authenticated={authenticated}
+              />
+              <AuthRoute
+                exact
+                path="/signup"
+                component={Signup}
+                authenticated={authenticated}
+              />
             </Switch>
           </div>
         </Router>
@@ -33,3 +57,4 @@ function App() {
 }
 
 export default App;
+
